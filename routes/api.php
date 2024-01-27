2568746db0cc92ea0a7f78986bbf81ff9554c720
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2024-01-28 00:33:31 +0300

    Added product-routes in api.php file

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products', 'index');
+    Route::get('/products/{id}', 'show');
+    Route::post('/products', 'store');
+    Route::put('/products/{id}', 'update');
+    Route::delete('/products/{id}', 'destroy');
 });
